7c1af0807b928fecbccf7de04f906d4f2855de0b
mohammadmahdishokraneh <mohammadmahdijavani1381@gmail.com>
2021-05-13 12:57:42 +0430

    updated

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct dictionary{
+    string word;
+    dictionary* nxt;
+    struct synonyms;
+};
+
+struct synonyms {
+    string synonym;
+    synonyms *nxt;
+};
+
+void add_word(string ,string);
+void delete_word(string);
+void delete_synonym(string);
+void show_word(string);
+void show_words();
+void change_spelling_of_word(string ,string);
+void words_storage();
+// void read_words(files name);
+
+int main() {
+
+    return 0;
+}
